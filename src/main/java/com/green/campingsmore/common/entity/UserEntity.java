152f55a7643a369af4edf4f467789549ef9eab3b
JeongHoBBang <dlawjdghbn@naver.com>
2023-08-24 12:50:27 +0900

    commit

---
+++ src/main/java/com/green/campingsmore/common/entity/UserEntity.java
@@ -0,0 +1,64 @@
+package com.green.campingsmore.common.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.green.campingsmore.common.config.jpa.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Table(name = "user",uniqueConstraints = {@UniqueConstraint(name = "unique_user_id",columnNames = {"userid"})})
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class UserEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false, nullable = false, columnDefinition = "BIGINT UNSIGNED", length = 20)
+    private Long iuser;
+
+    @Column(nullable = false, length = 20)
+    private String userId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @Column(nullable = false,length = 20, name = "name")
+    private String name;
+
+    @Column(nullable = false,length = 20)
+    private String birthDate;
+
+    @Column(nullable = false,length = 11)
+    private String phone;
+
+    @Column(nullable = false , length = 1, columnDefinition = "TINYINT CHECK(gender in (0,1))")
+    @ColumnDefault("0")
+    private Long gender;
+
+    @Column(nullable = false, length = 100)
+    private String userAddress;
+
+    @Column(length = 100)
+    private String userAddressDetail;
+
+    @Column(nullable = false, length = 10)
+    private String role;
+
+    @Column(length = 100)
+    private String pic;
+
+    @Column(length = 1, columnDefinition = "TINYINT")
+    @ColumnDefault("1")
+    @JsonIgnore
+    private int delyn;
+}
