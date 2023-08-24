0145d580de5cfcd0bea3002cdccbb8268c6b6df9
JeongHoBBang <dlawjdghbn@naver.com>
2023-08-24 14:59:17 +0900

    commit

---
+++ src/main/java/com/green/campingsmore/common/entity/BoardEntity.java
@@ -0,0 +1,51 @@
+package com.green.campingsmore.common.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.green.campingsmore.common.config.jpa.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@Table(name = "board")
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@SuperBuilder
+public class BoardEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false, nullable = false, columnDefinition = "BIGINT UNSIGNED", length = 20)
+    private Long iboard;
+
+    @JoinColumn(name = "iuser")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
+    private UserEntity userEntity;
+
+    @JoinColumn(name = "icategory")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
+    private BoardCategoryEntity boardCategoryEntity;
+
+    @Column(nullable = false, length = 20)
+    private String title;
+
+    @Column(nullable = false, length = 300)
+    private String ctnt;
+
+    @Column(nullable = false, columnDefinition = "TINYINT", length = 1)
+    @ColumnDefault("1")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Integer delYn;
+
+    @Column(updatable = false, nullable = false, columnDefinition = "BIGINT UNSIGNED", length = 20)
+    @ColumnDefault("0")
+    private Long boardView;
+}
