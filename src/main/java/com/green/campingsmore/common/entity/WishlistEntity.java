95eccece86946507d79a6bc0932ae2ce48079ae5
황주은 <rlahfld54@naver.com>
2023-08-24 17:03:37 +0900

    UserEntity와 WishlistEntity 수정 완료

--- src/main/java/com/green/campingsmore/common/entity/WishlistEntity.java
+++ src/main/java/com/green/campingsmore/common/entity/WishlistEntity.java
@@ -1,26 +1,25 @@
 package com.green.campingsmore.common.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.green.campingsmore.common.config.jpa.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "wishlist")
 @Data
 @NoArgsConstructor
-@EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 @SuperBuilder
-public class WishlistEntity extends BaseEntity {
+public class WishlistEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(updatable = false, nullable = false, columnDefinition = "BIGINT UNSIGNED", length = 20)
+    @Column(updatable = false, nullable = false, columnDefinition = "BIGINT UNSIGNED", length = 15)
     private Long iwish;
 
     @JoinColumn(name = "iuser")
@@ -31,7 +30,11 @@ public class WishlistEntity extends BaseEntity {
     @ManyToOne
     private ItemEntity itemEntity;
 
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "del_yn")
     @ColumnDefault("1")
-    @JsonIgnore
     private Integer delYn;
 }
