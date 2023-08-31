1e96abcd940b590f6c0257a6776d3c651a8bc5b9
장하은 <haun0@naver.com>
2023-08-31 09:36:59 +0900

    haeun - item 시작전

--- src/main/java/com/green/campingsmore/common/item/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.green.campingsmore.common.item;
-
-import com.green.campingsmore.common.entity.ItemEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<ItemEntity, Long> {
-}
